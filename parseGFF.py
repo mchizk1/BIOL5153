944c5676be5afb9e97cfd7696e2d2c55e7fbba4e
Mason Chizk <mchizk1@gmail.com>
2021-04-23 15:53:55 -0500

    updating the parseGFF script to now return CDS for each gene

--- parseGFF.py
+++ parseGFF.py
@@ -2,7 +2,10 @@
 
 import csv
 import argparse
+import re
+from collections import defaultdict
 from Bio import SeqIO
+from Bio.Seq import Seq
 
 #
 #inputs: 1) GFF file, 2) corresponding genome sequence (FASTA format)
@@ -19,8 +22,17 @@ args = parser.parse_args()
 
 # read in FASTA file
 genome = SeqIO.read(args.fasta, 'fasta')
-print(genome.id)
-print(len(genome.seq))
+#print(genome.id)
+#print(len(genome.seq))
+
+# create a function that returns the reverse complement of features on the '-' strand
+def rev_comp(mysequence, strand):
+	if(strand == "-"):
+		mysequence = mysequence.reverse_complement()
+	return(mysequence)
+
+# declare genes dictionary
+CDS_dict = defaultdict(dict)
 
 # read in GFF file
 with open(args.gff, 'r') as gff_in:
@@ -32,8 +44,23 @@ with open(args.gff, 'r') as gff_in:
 		start 	= line[3]
 		end 	= line[4]
 		strand 	= line[6]
-		print(genome.id, line[8])
-		print(genome.seq[(int(start)-1):int(end)])
-		print()
+		info	= line[8]
+		gene 	= re.findall("Gene\s(.*?)\s",info)
+		mysequence = genome.seq[(int(start)-1):int(end)]
+		if (line[2] == "CDS"):
+			if (CDS_dict[gene[0]] == {}):
+				CDS_dict[gene[0]] = [strand, mysequence]
+			else:
+				CDS_dict[gene[0]].append(mysequence)
 
-
+# this loops over the newly created dictionary to splice together coding sequences
+# and then call the rev_comp function for relevant sequences
+for line in CDS_dict:
+	print(">Citrullus_lanatus_"+line)
+	spacer = Seq("")
+	spliced=str("")
+	for seq in CDS_dict[line][1:]:
+		spliced += seq
+	CDS = rev_comp(spliced, CDS_dict[line][0])
+	print(CDS)
+	print()
